c42a7bc7f35b4248fe6d99f7f9472ebc0007a304
1430031264 <1430031264@qq.com>
2019-07-17 09:56:57 +0800

    redux 的 另一种用法

--- react/redux-count/src/App.js
+++ react/redux-count/src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import React from 'react';
+import { connect,  } from 'react-redux'
+import React, { Component } from 'react';
+class Count extends Component {
+  state = {  }
+  handleIn = () => {
+    // store.dispatch({ type: 'INCREMENT' })
+    this.props.increment();
+  }
+  handleDe = () => {
+    // store.dispatch({ type: 'DECREMENT' })
+    this.props.decrement();
+  }
+  handleAddFilm = () => {
+    // store.dispatch({ type: 'ADD_FILM',film: { name: '蜘蛛侠：英雄远征' } })
+    this.props.addFilm();
+  }
+  render() {
+    console.log(this.props)
+    const { count,films } = this.props
+    return ( 
+      <div>
+        count: { count}
+        <button onClick={this.handleIn}>+</button>
+        <button onClick={this.handleDe}>-</button>
+        <button onClick={this.handleAddFilm}>添加电影</button>
+        {
+          // .map((film,i) => (<li></li>) )
+          films.map((film,i) => {
+            return (
+              <li key={i}>{ film.name }</li>
+            )
+          })
+        }
+      </div>
+     );
+  }
 }
-
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    count: state.count,
+    films: state.films
+  }
+}
+const mapDispatchToPros = (dispatch) => {
+  return {
+    foo: () => { console.log('foo')},
+    increment: () => { dispatch({ type: 'INCREMENT' }) },
+    decrement: () => { dispatch({ type: 'DECREMENT' }) },
+    addFilm: () => {
+      dispatch({
+        type: 'ADD_FILM',
+        film: { name: '蜘蛛侠：英雄远征' }
+      })
+    }
+  }
+}
+export default connect(mapStateToProps,mapDispatchToPros)(Count);
