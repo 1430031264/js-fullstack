527ba596437414f3c580f40ee3e8d3d401c9ef1e
1430031264 <1430031264@qq.com>
2019-08-13 22:34:02 +0800

    解构、运算符

--- es6/es6test/dist/index.js
+++ es6/es6test/dist/index.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // var a = "张三";
 // console.log(a)
@@ -27,21 +27,66 @@
 // --------------- 解构赋值 -----------
 
 // ==========数组解构 ==============
-var a = 0,
-    b = 1,
-    c = 2;
-// 可以给变量增加默认值
+// let [a,b,c] = [0,1,2]
+// // 可以给变量增加默认值
+// let [foo = true] = []
+// console.log(a)
+// console.log(foo) // 输出 true
 
-var _ref = [],
-    _ref$ = _ref[0],
-    foo = _ref$ === undefined ? true : _ref$;
+// let [q,w = 'lei了lei了'] = ["张三"]
+// console.log(q + w)
 
-console.log(a);
-console.log(foo); // 输出 true
 
-var _ref2 = ["张三"],
-    q = _ref2[0],
-    _ref2$ = _ref2[1],
-    w = _ref2$ === undefined ? 'lei了lei了' : _ref2$;
+///  =======对象的解构 ==========
+// let { foo, bar} = { foo: '张三',bar: 'lei了lei了'}
+// console.log(foo + bar)
 
-console.log(q + w);
+// ============字符串的解构============
+// const [a,b,c,d,e] = 'hello';
+
+
+// ========= 对象扩展运算符==========
+var arr1 = ['a', 'b', 'c'];
+// let arr2 = arr1 // 修改前
+var arr2 = [].concat(arr1);
+console.log(arr2);
+arr2.push('d');
+console.log(arr1);
+// 这种情况下。当我们修改了arr2 的时候 arr1 也会跟着
+// 改变，但是我们并不想arr1 改变，此时就呢个用到对象扩展运算符(...)
+
+
+// =========== rest 运算符 （...）
+function foo(first) {
+  for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+    args[_key - 1] = arguments[_key];
+  }
+
+  // console.log(args.length)
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = args[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var val = _step.value;
+
+      console.log(val);
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator.return) {
+        _iterator.return();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
+}
+foo(1, 2, 3, 4, 6, 8, 7, 9);
+// 运用 当我们知道了前面固定数量的字符串长度之后，后面的长度我们不知道，此时可以个使用rest 运算符
--- es6/es6test/src/index.js
+++ es6/es6test/src/index.js
@@ -25,11 +25,41 @@
 // --------------- 解构赋值 -----------
 
 // ==========数组解构 ==============
-let [a,b,c] = [0,1,2]
-// 可以给变量增加默认值
-let [foo = true] = []
-console.log(a)
-console.log(foo) // 输出 true
+// let [a,b,c] = [0,1,2]
+// // 可以给变量增加默认值
+// let [foo = true] = []
+// console.log(a)
+// console.log(foo) // 输出 true
 
-let [q,w = 'lei了lei了'] = ["张三"]
-console.log(q + w)
+// let [q,w = 'lei了lei了'] = ["张三"]
+// console.log(q + w)
+
+
+///  =======对象的解构 ==========
+// let { foo, bar} = { foo: '张三',bar: 'lei了lei了'}
+// console.log(foo + bar)
+
+// ============字符串的解构============
+// const [a,b,c,d,e] = 'hello';
+
+
+// ========= 对象扩展运算符==========
+let arr1 = ['a','b','c']
+// let arr2 = arr1 // 修改前
+let arr2= [...arr1]
+console.log(arr2)
+arr2.push('d')
+console.log(arr1)
+// 这种情况下。当我们修改了arr2 的时候 arr1 也会跟着
+// 改变，但是我们并不想arr1 改变，此时就呢个用到对象扩展运算符(...)
+
+
+// =========== rest 运算符 （...）
+function foo(first, ...args) {
+  // console.log(args.length)
+  for(let val of args) {
+    console.log(val)
+  }
+}
+foo(1,2,3,4,6,8,7,9)
+// 运用 当我们知道了前面固定数量的字符串长度之后，后面的长度我们不知道，此时可以个使用rest 运算符
